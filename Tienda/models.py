661d6144a5787d9a943ee60bde020b1c9ce7b43e
IsaacSolisPadilla <isasolpad@alum.us.es>
2024-11-11 22:00:34 +0100

    Not working yet

--- Tienda/models.py
+++ Tienda/models.py
@@ -3,6 +3,18 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 
+class CustomUser(User):
+    direccion = models.CharField(max_length=100)
+
+class DatosEmpresa(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.TextField()
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
 class CategoriaProducto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True)
@@ -23,25 +35,42 @@ class Producto(models.Model):
 
 class Pedido(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    fecha = models.DateTimeField(auto_now_add=True)
-    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    fecha_pedido = models.DateTimeField(auto_now_add=True)
+    fecha_salida = models.DateTimeField()
+    fecha_entrega = models.DateTimeField()
+    direccion = models.TextField()
+    estado = models.CharField(
+        max_length=50,
+        choices=[("Pendiente", "Pendiente"), ("Enviado","Enviado") ,("Entregado","Entregado")]
+    )
+    metodo_de_pago = models.CharField(
+        max_length=50,
+        choices=[("Contrareembolso", "Contrareembolso"), ("Pasarela","Pasarela de pago")]
+    )
+    coste_envio = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"Pedido {self.id} de {self.usuario.username}"
-
-class LineaFactura(models.Model):
-    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name="lineas_factura")
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    cantidad = models.PositiveIntegerField()
-    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.cantidad} de {self.producto.nombre} en Pedido {self.pedido.id}"
-
+    
 class Factura(models.Model):
     pedido = models.OneToOneField(Pedido, on_delete=models.CASCADE)
-    fecha_emision = models.DateTimeField(auto_now_add=True)
-    numero_factura = models.CharField(max_length=50, unique=True)
+
+    def precio_total(self):
+        return sum(map(lambda linea: linea.precio_linea() ,LineaFactura.objects.filter(factura=self).all()))
+    
+    def __str__(self):
+        return f"Factura {self.id} para Pedido {self.pedido.id}"
+
+class LineaFactura(models.Model):
+    
+    factura = models.ForeignKey(Factura, on_delete=models.CASCADE, related_name="lineas_factura", null=True)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
+
+    def precio_linea(self):
+        return self.producto.precio * self.cantidad
 
     def __str__(self):
-        return f"Factura {self.numero_factura} para Pedido {self.pedido.id}"
+        return f"{self.cantidad} de {self.producto.nombre} en Pedido {self.factura.pedido.id}"
+
+
