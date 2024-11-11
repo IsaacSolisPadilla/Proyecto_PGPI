0da53cb530df259b1c3995a16746c27d6f797e7d
IsaacSolisPadilla <isasolpad@alum.us.es>
2024-11-11 19:47:01 +0100

    commit

--- Tienda/models.py
+++ Tienda/models.py
@@ -12,12 +12,13 @@ class CategoriaProducto(models.Model):
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(CategoriaProducto, on_delete=models.CASCADE, related_name="productos")
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
+    descripcion = models.TextField(blank=True)
+    fotografia = models.ImageField(upload_to='productos/')
 
-    def __str__(self):
+    def str(self):
         return self.nombre
 
 class Pedido(models.Model):
