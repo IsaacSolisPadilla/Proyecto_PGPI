597b8b3a50d518905292a9a10ae49b990ad6b24e
Marina Calero <73230580+marcallop7@users.noreply.github.com>
2024-11-24 19:03:16 +0100

    Creando pantalla base.html y mejorando pantallas

--- Tienda/Producto/views.py
+++ Tienda/Producto/views.py
@@ -43,7 +43,8 @@ def actualizar_producto(request, producto_id):
 
 def detalle_producto(request, producto_id):
     producto = get_object_or_404(Producto, id=producto_id)
-    return render(request, 'Productos/detalle_producto.html', {'producto': producto})
+    categorias = CategoriaProducto.objects.all()
+    return render(request, 'Productos/detalle_producto.html', {'producto': producto, 'categorias': categorias})
 
 def detalle_carro_pago(request, producto_id):
     producto = get_object_or_404(Producto, id=producto_id)
@@ -56,3 +57,34 @@ def lista_productos(request):
     else:
         productos = Producto.objects.all()
     return render(request, 'Productos/lista_productos.html', {'productos': productos})
+
+def crear_categoria_de_producto(request):
+    if request.method == 'POST':
+        data = request.POST
+        categoriaProducto = ProductoService.crear_categoria_de_producto(data)
+        return redirect('lista_categorias')
+    else:
+        categorias = CategoriaProducto.objects.all()
+        return render(request, 'Productos/crear_categoria_producto.html',{'categorias': categorias} )
+
+@user_passes_test(lambda u: u.is_superuser)
+def eliminar_categoria_de_producto(request, categoria_de_producto_id):
+    ProductoService.eliminar__categoria_de_producto(categoria_de_producto_id)
+    return redirect('lista_categorias')
+
+@user_passes_test(lambda u: u.is_superuser)
+def ver_actualizar_categoria_de_producto(request, categoria_de_producto_id):
+    categoria = get_object_or_404(CategoriaProducto, id=categoria_de_producto_id)
+    return render(request, 'Productos/actualizar_categoria.html', {'categoria': categoria})
+
+@user_passes_test(lambda u: u.is_superuser)
+def actualizar_categoria_de_producto(request, categoria_de_producto_id):   
+    data = request.POST
+    ProductoService.actualizar_categoria_de_producto(categoria_de_producto_id, data)
+    return redirect('lista_categorias')
+
+def lista_productos_de_categoria(request, categoria_id):
+    categoria = get_object_or_404(CategoriaProducto, id=categoria_id)
+    productos = Producto.objects.filter(categoria=categoria)
+    categorias = CategoriaProducto.objects.all()
+    return render(request, 'Productos/lista_productos_categoria.html', {'categorias': categorias, 'categoria': categoria, 'productos': productos})
