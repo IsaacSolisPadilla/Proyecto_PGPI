9e4b68194bee4859d5301a1dddfb8479073b6c06
dangalcan <megamagolas@gmail.com>
2024-11-25 12:51:56 +0100

    update instalation .sh

--- instalation.sh
+++ instalation.sh
@@ -1,4 +1,5 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata ./Tienda/fixtures/datos_prueba.json
+python manage.py createsuperuser #username: admin, email: admin@admin.com, password: admin
 python manage.py runserver
