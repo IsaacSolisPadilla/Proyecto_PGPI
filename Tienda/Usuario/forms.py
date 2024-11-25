40e24d83d2fcae503414e13273562caf80e4660f
luchersol <lmherencia2003@gmail.com>
2024-11-25 17:17:41 +0100

    Inicialización de dirección y metodo de pago factura y posibilidad de cambiar dirección y metodo de pago predeterminados de usuario

--- Tienda/Usuario/forms.py
+++ Tienda/Usuario/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordChangeForm
+from django.utils.safestring import mark_safe
+
+from Tienda.models import Datos
 
 class EditProfileForm(forms.ModelForm):
     class Meta:
@@ -18,6 +21,25 @@ class EditProfileForm(forms.ModelForm):
         if User.objects.filter(username=username).exclude(pk=self.instance.pk).exists():
             raise forms.ValidationError('El nombre de usuario ya está en uso.')
         return username
+    
+class EditDatosUserForm(forms.ModelForm):
+    def as_custom(self):
+        output = []
+        for field in self.visible_fields():
+            output.append(f'<label>{field} <span>{field.label}</span></label>')
+        return mark_safe('\n'.join(output))
+    class Meta:
+        model = Datos
+        fields = ['direccion', 'metodo_de_pago']
+        widgets = {
+            'direccion': forms.TextInput(attrs={
+                'class': 'input'
+            }),
+            'metodo_de_pago': forms.Select(attrs={
+                'class': 'input',
+                'requerided': False
+            }),
+        }
 
 class EditPasswordForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
